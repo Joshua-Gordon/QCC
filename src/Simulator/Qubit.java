eeb681fb4d4aca75b17dc09e228bb9edf6ffc268
MassiCutug <cutugnma@clarkson.edu>
2018-07-13 23:35:13 -0400

    Updated References to Matrix from restructuring

--- src/Simulator/Qubit.java
+++ src/Simulator/Qubit.java
@@ -9,7 +9,7 @@ import java.util.Random;
 public class Qubit extends Vector<Complex> {
 
     public Qubit(Complex a, Complex b) {
-        super(Complex.ZERO(), a,b);
+        super(a,b);
     }
 
     public static Qubit ONE() {
