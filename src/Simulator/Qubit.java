17c63f18710c7aaae3000d9a72deba34266509d1
Josh Gordon <joshua.sam.gordon@gmail.com>
2018-07-27 23:18:49 -0400

    Fix measurement bug

--- src/Simulator/Qubit.java
+++ src/Simulator/Qubit.java
@@ -39,9 +39,17 @@ public class Qubit extends Vector<Complex> {
             return -1;
         }
         int measured = 0;
-        for(int i = probs.size()-1; i >= 0; --i) {
-            measured += (new Random()).nextDouble() < probs.get(i) ? 1 << i : 0;
+        double rand = (new Random()).nextDouble();
+        for(int i = 0; i < probs.size(); ++i) {
+            rand -= probs.get(i);
+            if(rand <= 0) {
+                measured = i;
+                break;
+            }
         }
+        //for(int i = probs.size()-1; i >= 0; --i) {
+        //    measured += (new Random()).nextDouble() < probs.get(i) ? 1 << i : 0;
+        //}
         return measured; //possible bug, may need to bitshift down 1, as 0001 should be zero.
     }
 
